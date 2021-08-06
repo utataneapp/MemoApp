59057d35c03b870989faea1b2122fde4ec580bf1
utatanetaro <87312404+utatanetaro@users.noreply.github.com>
2021-08-07 07:43:11 +0900

    Install Eslint

--- App.jsx
+++ App.jsx
@@ -6,7 +6,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* 警告を１行だけ無効化 */}
+      {/*eslint-disable*/}
+      <StatusBar style="auto" />\
     </View>
   );
 }
