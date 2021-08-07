4d6a94d740769fcc606d219e484bde21d71aa1a1
utatanetaro <87312404+utatanetaro@users.noreply.github.com>
2021-08-07 10:56:01 +0900

    Create Hello components

--- App.jsx
+++ App.jsx
@@ -2,13 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello style={{ fontSize: 16 }}>World</Hello>
       <Text>Open up App.js to start working on your app!</Text>
-      {/* 警告を１行だけ無効化 */}
-      {/*eslint-disable*/}
-      <StatusBar style="auto" />\
+      {/* eslint-disable-next-line */}
+      <StatusBar style="auto" />
     </View>
   );
 }
